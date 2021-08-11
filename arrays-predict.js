40bf2b80cfdc7a9492ccda64e5566cffabd7e6fd
Joseph Hooker <jcolehooker@gmail.com>
2021-08-11 11:15:18 -0500

    final submission ready

--- arrays-predict.js
+++ arrays-predict.js
@@ -1,19 +1,19 @@
-// var arr = [8,6,7,5,3,0,9]
-// for(var i = 0; i < arr.length; i++){    
-//     console.log(arr[i]);
-// }
+var arr = [8,6,7,5,3,0,9]
+for(var i = 0; i < arr.length; i++){    
+    console.log(arr[i]);
+}
 
 //output: 8 6 7 5 3 0 9
 
-// var arr = [7,3,8,4,2,0,1];
-// for(var i = 0; i < arr.length; i++){ 
-//     if(arr[i] % 2 == 0){
-//         console.log(arr[i]);
-//     } 
-//     else{
-//         console.log("That is odd!");
-//     }
-// }
+var arr = [7,3,8,4,2,0,1];
+for(var i = 0; i < arr.length; i++){ 
+    if(arr[i] % 2 == 0){
+        console.log(arr[i]);
+    } 
+    else{
+        console.log("That is odd!");
+    }
+}
 
 //output: That is odd! That is odd! 8 4 2 0 That is odd!
 
